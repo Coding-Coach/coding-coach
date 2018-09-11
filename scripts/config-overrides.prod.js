4bae8cada281f6bc67274458543fe7cf70d75164
Jagadish K <k.jagdish@gmail.com>
2018-09-11 20:14:56 +0530

    Use node-sass with webpack to avoid the need of having to run sass compile separately.

---
+++ scripts/config-overrides.prod.js
@@ -0,0 +1,15 @@
+const path = require('path');
+
+module.exports = function(config) {
+  // Use your own ESLint file
+  let eslintLoader = config.module.rules[0];
+  eslintLoader.use[0].options.useEslintrc = true;
+
+  // Add the SASS loader second-to-last
+  // (last one must remain as the "file-loader")
+  let loaderList = config.module.rules[1].oneOf;
+  loaderList.splice(loaderList.length - 1, 0, {
+    test: /\.scss$/,
+    use: ["style-loader", "css-loader", "sass-loader"]
+  });
+}
