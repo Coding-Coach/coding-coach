1b025a4edbddb837f9512c6d04c29909309f6400
Hakan Karaduman <karadumanhakan1@gmail.com>
2018-09-11 13:30:03 +0300

    forgot to add href and target attributes back to a tag, :embarrassed

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ class App extends Component {
         <header className="App__header">
           <h1 className="App__title">Coding Coach</h1>
           <h4 className="App__tagline">Connecting developers with mentors worldwide.</h4>
-          <a className="button button__white--fill button__link">
+          <a className="button button__white--fill button__link" href="https://docs.google.com/forms/d/1TraoygcJay39cLiBQM-ROlToy7xIgcdx_VvCQuE1lJw/edit#responses" target="_blank">
             Get Started
           </a>
         </header>
