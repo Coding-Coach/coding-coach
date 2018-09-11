3ecded6566956fc4e26f871d0ee5ecec6a5427c4
Hakan Karaduman <karadumanhakan1@gmail.com>
2018-09-11 12:06:25 +0300

    changed click action from a tag to button, so that user does not have to click text

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+  onGetStarted = () => {
+    const url = 'https://docs.google.com/forms/d/1TraoygcJay39cLiBQM-ROlToy7xIgcdx_VvCQuE1lJw/edit#responses';
+    window.open(url, '_blank');
+  }
+
   render() {
     return (
       <div className="App">
@@ -12,12 +17,8 @@ class App extends Component {
         <header className="App__header">
           <h1 className="App__title">Coding Coach</h1>
           <h4 className="App__tagline">Connecting developers with mentors worldwide.</h4>
-          <button className="button button__white--fill">
-            <a 
-              href="https://docs.google.com/forms/d/1TraoygcJay39cLiBQM-ROlToy7xIgcdx_VvCQuE1lJw/edit#responses"
-              target="blank"
-              className="button__link"
-            >
+          <button className="button button__white--fill" onClick={this.onGetStarted}>
+            <a className="button__link">
               Get Started
             </a>
           </button>
