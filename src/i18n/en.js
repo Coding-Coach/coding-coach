6e2537141797855c1c68878e9d9ba8fd19a9af6a
Halkreen <thomas.marcelin.07@gmail.com>
2018-09-12 18:43:00 +0200

    Feature/i18n support (duplicates from PR#9) (#24)
    * feat(i18n): added react-i18next dependency
    
    * feat(i18n): added scaffolding and landing page english keys
    
    * feat(i18n): added i18next and landing page translations in i18n.js (might need individual file)
    
    * feat(i18n): added fr/en translations for landing page
    
    * chore(i18n): changed imports location and created individual js files for translations
    
    * chore(i18n): changed double quotes to single quotes for javascript
    
    * chore(i18n): Changed gitignore for merge conflicts
    
    * chore: package.lock was merged incorrectly

---
+++ src/i18n/en.js
@@ -0,0 +1,9 @@
+export default function loadEn() {
+  return {
+    translations: {
+      'landing-page-header': 'Coding Coach',
+      'landing-page-start': 'Get Started',
+      'landing-page-subheader': 'Connecting developers with mentor worldwide'
+    }
+  };
+}
---
+++ src/i18n/fr.js
@@ -0,0 +1,9 @@
+export default function loadFr() {
+  return {
+    translations: {
+      'landing-page-header': 'Coding Coach',
+      'landing-page-start': 'Commencez maintenant',
+      'landing-page-subheader': 'Apprenez avec des mentors partout à travers le monde'
+    }
+  };
+}
