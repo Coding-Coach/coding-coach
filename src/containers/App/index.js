b7c4d9638d64453099fdccbb26da7999078d0dc1
Crysfel Villa <crysfel@bleext.com>
2018-09-12 15:59:36 -0500

    Using the decorator for the translations

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,35 +1,27 @@
 import React from 'react';
-import { I18n } from 'react-i18next';
+import { translate } from 'react-i18next';
 import 'i18n/i18n';
 import styles from './styles.scss';
 
-function App() {
+export function App({ t }) {
   return (
-    <I18n ns="translations">
-      {t => (
-      <div className={styles.app}>
-        <img 
-          src={require('images/mentor.jpg')} 
-          alt="Mentor" 
-          className={styles.image}
-        />
-        <header className={styles.header}>
-          <h1 className={styles.title}>{t('landing-page-header')}</h1>
-          <h4 className={styles.tagline}>{t('landing-page-subheader')}</h4>
-          {/* TODO: create a React Component for a `Button` and add scoped styles */}
-          <a
-            className="button button__white--fill button__link"
-            href="https://docs.google.com/forms/d/1TraoygcJay39cLiBQM-ROlToy7xIgcdx_VvCQuE1lJw/edit#responses"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            {t('landing-page-start')}
-          </a>
-        </header>
-      </div>
-      )}
-    </I18n>
+    <div className={styles.app}>
+      <img src={require('./mentor.jpg')} alt="Mentor" className={styles.image} />
+      <header className={styles.header}>
+        <h1 className={styles.title}>{t('landing-page-header')}</h1>
+        <h4 className={styles.tagline}>{t('landing-page-subheader')}</h4>
+        {/* TODO: create a React Component for a `Button` and add scoped styles */}
+        <a
+          className="button button__white--fill button__link"
+          href="https://docs.google.com/forms/d/1TraoygcJay39cLiBQM-ROlToy7xIgcdx_VvCQuE1lJw/edit#responses"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          {t('landing-page-start')}
+        </a>
+      </header>
+    </div>
   );
 }
 
-export default App;
+export default translate('translations')(App);
--- src/index.js
+++ src/index.js
@@ -3,8 +3,7 @@ import ReactDOM from 'react-dom';
 import App from 'containers/App';
 import registerServiceWorker from 'registerServiceWorker';
 // @TODO: Remove this after creating the `Button` component
-import 'styles/scss/index.scss';
-
+import 'theme/index.scss';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
