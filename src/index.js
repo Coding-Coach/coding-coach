7f5734d16009682575549112ca4e4dcd785de498
NitishPhanse <nitsums@gmail.com>
2018-09-18 09:37:56 +0530

    incorrect app js reference

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from 'containers/App';
+import App from 'containers/App/App';
 import registerServiceWorker from 'registerServiceWorker';
 // @TODO: Remove this after creating the `Button` component
 import 'theme/index.scss';
