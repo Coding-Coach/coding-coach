4cce13503183cc4ebfe8ca00f35f21e3d2a9bdd8
Praphul <praphul.sinha@outlook.com>
2018-09-21 11:12:32 +0530

    #57 Added unit test cases for contact and social media components

--- src/components/contact/__tests__/Contact.test.jsx
+++ src/components/contact/__tests__/Contact.test.jsx
@@ -3,7 +3,9 @@ import { render } from 'react-testing-library';
 import Contact from '../Contact';
 
 test('renders the contact page', () => {
-  const { getByText } = render(<Contact />);
+  const { getByText, getByAltText } = render(<Contact />);
 
   expect(getByText('Contact')).toBeInTheDocument();
+  expect(getByAltText('people search logo')).toBeInTheDocument();
+  expect(getByText(process.env.REACT_APP_EMAIL)).toBeInTheDocument();
 });
