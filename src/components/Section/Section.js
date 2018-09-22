ecb8826722721ebed55dd315da8678d03d1fc326
Victor Ribero <victor.ribero3@gmail.com>
2018-09-22 14:52:58 +0200

    First proposal. Needs discussion

--- src/components/Section/Section.js
+++ src/components/Section/Section.js
@@ -2,15 +2,21 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './Section.scss';
 
-const Section = ({ children }) => <section className={styles.section}>{children}</section>;
+const Section = ({ backgroundColor, paddingVertical, paddingHorizontal, children }) => (
+  <section className={styles.section}>{children}</section>
+);
 
 Section.propTypes = {
   backgroundColor: PropTypes.oneOf(['transparent', 'white', 'grey', 'watercolor']),
+  paddingVertical: PropTypes.bool,
+  paddingHorizontal: PropTypes.bool,
   children: PropTypes.node.isRequired,
 };
 
 Section.defaultProps = {
   backgroundColor: 'transparent',
+  paddingVertical: true,
+  paddingHorizontal: true,
 };
 
 export default Section;
