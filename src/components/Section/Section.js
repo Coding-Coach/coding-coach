1d7747537a721b14c8263f09979899338adbab5e
Victor Ribero <victor.ribero3@gmail.com>
2018-09-22 17:08:36 +0200

    Solving how to generate styles by props with CSS Modules.

--- src/components/Section/Section.js
+++ src/components/Section/Section.js
@@ -2,9 +2,21 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './Section.scss';
 
-const Section = ({ backgroundColor, paddingVertical, paddingHorizontal, children }) => (
-  <section className={styles.section}>{children}</section>
-);
+const Section = ({ backgroundColor, paddingVertical, paddingHorizontal, children, className }) => {
+  return (
+    <section
+      className={[
+        styles.section,
+        backgroundColor ? $`${styles}.is${backgroundColor}` : '',
+        paddingVertical ? styles.hasPaddingHorizontal : '',
+        paddingHorizontal ? styles.hasPaddingVertical : '',
+        className,
+      ].join(' ')}
+    >
+      {children}
+    </section>
+  );
+};
 
 Section.propTypes = {
   backgroundColor: PropTypes.oneOf(['transparent', 'white', 'grey', 'watercolor']),
--- src/components/Section/Section.stories.js
+++ src/components/Section/Section.stories.js
@@ -3,16 +3,40 @@ import { storiesOf } from '@storybook/react';
 
 import Section from './Section';
 
+const LoremIpsum = () => (
+  <p>
+    Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum
+    ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N.
+    del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló
+    de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino
+    que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente
+    igual al original. Fue popularizado en los 60s con la creación de las hojas "Letraset", las
+    cuales contenian pasajes de Lorem Ipsum, y más recientemente con software de autoedición, como
+    por ejemplo Aldus PageMaker, el cual incluye versiones de Lorem Ipsum.
+  </p>
+);
 storiesOf('Section', module)
-  .add('Transparent background', () => {
-    return <Section>works!</Section>;
-  })
-  .add('White background', () => {
-    return <Section>works!</Section>;
-  })
+  .add('Transparent background', () => (
+    <Section>
+      <LoremIpsum />
+    </Section>
+  ))
+  .add('White background', () => (
+    <Section backgroundColor="white">
+      <LoremIpsum />
+    </Section>
+  ))
   .add('Grey background', () => {
-    return <Section>works!</Section>;
+    return (
+      <Section backgroundColor="grey">
+        <LoremIpsum />
+      </Section>
+    );
   })
   .add('Water Main color background', () => {
-    return <Section>works!</Section>;
+    return (
+      <Section backgroundColor="watercolor">
+        <LoremIpsum />
+      </Section>
+    );
   });
