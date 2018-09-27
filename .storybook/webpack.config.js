cfa30ecdd0942b15eb86f3098b1b0494f59d837c
Raghuram Iyer <riyer@fleetstudio.com>
2018-09-28 00:14:46 +0530

    fixed the svgs not loading in storybook issue

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -8,7 +8,11 @@ module.exports = (storybookBaseConfig, configType) => {
       include: path.resolve(__dirname, '..', 'src'),
     },
     {
-      test: /\.(png|jpg|gif)$/,
+      
+      // test: /\.(png|jpg|gif)$/,
+      /** the above line caused svgs to not load when we are running storyboard.
+       *  So added svg along with some font formats just in case. */
+      test: /\.(woff|woff2|eot|ttf|svg|gif|png|jpg)$/,
       use: [
         {
           loader: 'file-loader',
