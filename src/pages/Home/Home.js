2f41160674ab12ca056bad00a9148f38a7cb0869
Victor Ribero <victor.ribero3@gmail.com>
2018-09-30 16:56:54 +0200

    Remove the onClick in Hero CTA

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -11,12 +11,6 @@ import Contact from 'components/contact/Contact';
 
 const heroImage = require('assets/logo.png');
 
-function handleOnCTAClick() {
-  document.getElementById('about').scrollIntoView({
-    behavior: 'smooth',
-  });
-}
-
 function Home({ t }) {
   return (
     <React.Fragment>
@@ -30,9 +24,7 @@ function Home({ t }) {
               <span>{t('Coach')}</span>
             </h1>
             <h2>{t('Connecting developers with mentors worldwide')}</h2>
-            <button onClick={handleOnCTAClick} className={styles.callToAction}>
-              {t('Learn More')}
-            </button>
+            <button className={styles.callToAction}>{t('Learn More')}</button>
           </header>
           <img className={styles.heroImage} alt="coding coach logo" src={heroImage} />
         </div>
