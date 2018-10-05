7f7b523c4cc8b35f46b05cbc6389cc1bbf1b3cdc
dio <dhrubesh97@gmail.com>
2018-10-05 14:23:48 +0530

    added menu bar for mobile view

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,9 +1,48 @@
 import React from 'react';
 import { translate } from 'react-i18next';
 import 'i18n/i18n';
+import { slide as Menu } from 'react-burger-menu';
 import styles from './navbar.scss';
 
 const Navbar = ({ t }) => {
+  const BurgerMenustyles = {
+    bmBurgerButton: {
+      position: 'fixed',
+      width: '36px',
+      height: '30px',
+      left: '36px',
+      top: '36px',
+    },
+    bmBurgerBars: {
+      background: '#21785b',
+    },
+    bmCrossButton: {
+      height: '24px',
+      width: '24px',
+    },
+    bmCross: {
+      background: '#979797',
+    },
+    bmMenu: {
+      background: '#afe9d6',
+      padding: '2.5em 1.5em 0',
+      fontSize: '1.15em',
+    },
+    bmMorphShape: {
+      fill: '#373a47',
+    },
+    bmItemList: {
+      color: '#b8b7ad',
+      padding: '0.8em',
+    },
+    bmItem: {
+      display: 'block',
+      padding: '1.2em',
+    },
+    bmOverlay: {
+      background: 'rgba(0, 0, 0, 0.3)',
+    },
+  };
   const renderDesktop = (
     <nav className={styles.navigationBar}>
       <div className={styles.inner}>
@@ -19,7 +58,21 @@ const Navbar = ({ t }) => {
       </div>
     </nav>
   );
-  const renderMobile = <div>This is for mobile.</div>;
+  const renderMobile = (
+    <div>
+      <Menu styles={BurgerMenustyles} isOpen={false}>
+        <a href="#about" className={styles.navLinks}>
+          {t('About')}
+        </a>
+        <a href="#mission" className={styles.navLinks}>
+          {t('Mission')}
+        </a>
+        <a href="#contact" className={styles.navLinks}>
+          {t('Contact')}
+        </a>
+      </Menu>
+    </div>
+  );
   const renderComponent = window.innerWidth > 600 ? renderDesktop : renderMobile;
 
   return <div>{renderComponent}</div>;
