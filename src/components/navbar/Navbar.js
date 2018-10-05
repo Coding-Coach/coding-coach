f12187505ee9c9989a455f397f37e23fe0c6586f
dio <dhrubesh97@gmail.com>
2018-10-05 11:46:37 +0530

    different renders for different views

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -3,20 +3,26 @@ import { translate } from 'react-i18next';
 import 'i18n/i18n';
 import styles from './navbar.scss';
 
-const Navbar = ({ t }) => (
-  <nav className={styles.navigationBar}>
-    <div className={styles.inner}>
-      <a href="#about" className={styles.navLinks}>
-        {t('About')}
-      </a>
-      <a href="#mission" className={styles.navLinks}>
-        {t('Mission')}
-      </a>
-      <a href="#contact" className={styles.navLinks}>
-        {t('Contact')}
-      </a>
-    </div>
-  </nav>
-);
+const Navbar = ({ t }) => {
+  const renderDesktop = (
+    <nav className={styles.navigationBar}>
+      <div className={styles.inner}>
+        <a href="#about" className={styles.navLinks}>
+          {t('About')}
+        </a>
+        <a href="#mission" className={styles.navLinks}>
+          {t('Mission')}
+        </a>
+        <a href="#contact" className={styles.navLinks}>
+          {t('Contact')}
+        </a>
+      </div>
+    </nav>
+  );
+  const renderMobile = <div>This is for mobile.</div>;
+  const renderComponent = window.innerWidth > 600 ? renderDesktop : renderMobile;
+
+  return <div>{renderComponent}</div>;
+};
 
 export default translate('translations')(Navbar);
