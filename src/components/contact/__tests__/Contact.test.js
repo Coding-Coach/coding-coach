1747b704005fbffa7cb9feaab0f3dd36bf642d06
Abhishek Prakash <abhishekabby@gmail.com>
2018-10-07 14:14:03 +0530

    Updated alt text and fixed failing test case for contact

--- src/components/contact/__tests__/Contact.test.js
+++ src/components/contact/__tests__/Contact.test.js
@@ -3,9 +3,10 @@ import { render } from 'react-testing-library';
 import Contact from '../Contact';
 
 test('renders the contact component', () => {
+  const altText = 'Person using a magnifying glass to see the details of another guy';
   const { getByText, getByAltText } = render(<Contact />);
 
   expect(getByText('Contact')).toBeInTheDocument();
-  expect(getByAltText('people search logo')).toBeInTheDocument();
+  expect(getByAltText(altText, { exact: false })).toBeInTheDocument();
   expect(getByText(process.env.REACT_APP_EMAIL)).toBeInTheDocument();
 });
