e808d87ef2380b3bbf96e6a2acac08b3ce5bd9c0
NitishPhanse <nitsums@gmail.com>
2018-10-09 01:00:56 +0530

    stories for buttons

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,26 +1,21 @@
 import React from 'react';
 import styles from './button.scss';
 import classNames from 'classnames';
-console.log(styles);
-
 /**
  *
  * @param {children} props
  * @param {type} props  primary, danger, secondary
- * @param {size} props  default, large, small, xsmall
+ * @param {size} props  default, large, small,
  * @param {disabled} props  true, false
- * @param {className} props  Custom Class
  * @param {fullWidth} props Button width is 100% of parent
  */
 const Button = (props) => {
-  const { children, type, size, disabled, className, fullWidth, ...rest } = props;
-  console.log(props);
+  const { children, type, size, disabled, fullWidth, ...rest } = props;
   const classes = classNames(
     styles.button,
     styles[type] || styles.primary,
-    styles[size],
+    styles[size] || styles.default,
     fullWidth && styles.fullWidth,
-    className,
   );
   return (
     <button disabled={disabled} {...rest} className={classes}>
--- src/components/Button/stories/Button.stories.js
+++ src/components/Button/stories/Button.stories.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebook, faTwitter, faInstagram } from '@fortawesome/free-brands-svg-icons';
 import Container from './container-story';
 
 import Button from './../';
-
+import styles from './styles.scss';
 storiesOf('Buttons', module).add('Button Types', () => (
   <Container title="Button Types">
     <Button disabled>Disabled Button</Button>
@@ -14,10 +16,66 @@ storiesOf('Buttons', module).add('Button Types', () => (
 ));
 
 storiesOf('Buttons', module).add('Button Sizes', () => (
-  <Container title="Button Types">
-    <Button disabled>Disabled Button</Button>
-    <Button type="danger">Danger Button</Button>
+  <div>
+    <Container title="Primary Button Sizes">
+      <Button>Default Size</Button>
+      <Button size="large">Large Button</Button>
+      <Button size="small">Small Button</Button>
+    </Container>
 
-    <Button>Secondary Button</Button>
-  </Container>
+    <Container title="Secondary Button Sizes">
+      <Button type="secondary">Default Size</Button>
+      <Button type="secondary" size="large">
+        Large Button
+      </Button>
+      <Button type="secondary" size="small">
+        Small Button
+      </Button>
+    </Container>
+
+    <Container title="Danger Button Sizes">
+      <Button type="danger">Default Size</Button>
+      <Button type="danger" size="large">
+        Large Button
+      </Button>
+      <Button type="danger" size="small">
+        Small Button
+      </Button>
+    </Container>
+
+    <Container title="Disabled Button Sizes">
+      <Button disabled>Default Size</Button>
+      <Button disabled size="large">
+        Large Button
+      </Button>
+      <Button disabled size="small">
+        Small Button
+      </Button>
+    </Container>
+
+    <Container title="Full Width Buttons">
+      <Button type="primary" fullWidth>
+        Default Size
+      </Button>
+      <Button type="secondary" fullWidth>
+        Default Size
+      </Button>
+      <Button type="danger" fullWidth>
+        Default Size
+      </Button>
+    </Container>
+  </div>
+));
+
+storiesOf('Buttons', module).add('Button Composites', () => (
+  <div>
+    <Container title="Button Composites">
+      <Button>
+        <FontAwesomeIcon icon={faTwitter} size="2x" />
+        With Some Icon
+      </Button>
+
+      <Button onClick={() => alert('You clickedMe')}>Click</Button>
+    </Container>
+  </div>
 ));
