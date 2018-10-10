42a66c8da337f6f5aed38ef1aae89c3fc183365c
Lena Venable <venablena@gmail.com>
2018-10-10 16:32:07 -0700

    make into a stateful component and use local strorage to check if cookies have been accepted

--- src/components/overlayCookie/overlayCookie.js
+++ src/components/overlayCookie/overlayCookie.js
@@ -1,27 +1,56 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { translate } from 'react-i18next';
 import 'i18n/i18n';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTimes } from '@fortawesome/free-solid-svg-icons';
 import styles from './overlayCookie.scss';
 
-const OverlayCookie = ({ t }) => {
-  return (
-    <section id={styles.cookieWrapper}>
-      <div className={styles.cookieContent}>
-        <p className={styles.cookieText}>
-          {t('cookie-notification') + " "}
-          <span>
-            <a href="#cookies">{t('cookie-link')}</a>
-          </span>
-        </p>
-        <p className={styles.cookieIcon}>
-          <FontAwesomeIcon
-            icon={faTimes} />
-        </p>
-      </div>
-    </section>
-  );
+class OverlayCookie extends Component {
+  state = {
+    cookiesAccepted: 'false'
+  };
+
+  componentDidMount(){
+    this.checkCookiePermission();
+  }
+
+  render() {
+    const { cookiesAccepted } = this.state;
+    if(cookiesAccepted === true) return null;
+
+    return (
+      <section id={styles.cookieWrapper}>
+        <div className={styles.cookieContent}>
+          <p className={styles.cookieText}>
+            To help personalize content, tailor and measure ads, and provide a safer experience, we use cookies. By clicking or navigating the site, you agree to allow our collecion of information on and off Coding Coach through cookies. Learn more, including about available controls:
+            {" "}
+            {/*t('cookie-notification') + " "*/}
+            <span>
+              <a href="#cookies">{/*t('cookie-link')*/}Cookies Policy</a>
+            </span>
+          </p>
+          <p className={styles.cookieIcon}>
+            <FontAwesomeIcon
+              icon={faTimes}
+              onClick={this.acceptCookies}
+              />
+          </p>
+        </div>
+      </section>
+    );
+  }
+
+  acceptCookies = () => {
+    if(!localStorage.hasOwnProperty('codingCoachCookies')) {
+      localStorage.setItem('codingCoachCookies', true);
+    }
+    this.setState({ cookiesAccepted: true });
+  }
+
+  checkCookiePermission = () => {
+    const hasCookiePermission = localStorage.getItem('codingCoachCookies')
+    if(hasCookiePermission) this.acceptCookies()
+  }
 };
 
 export default translate('translations')(OverlayCookie);
