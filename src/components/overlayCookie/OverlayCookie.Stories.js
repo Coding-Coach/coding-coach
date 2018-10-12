c9fa41ff9dacb7ef70b4bc421eb77c784d191382
Lena Venable <venablena@gmail.com>
2018-10-12 13:46:32 -0700

    add to storybook

---
+++ src/components/overlayCookie/OverlayCookie.Stories.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+
+import OverlayCookie from './OverlayCookie';
+
+storiesOf('OverlayCookie', module).add('renders an overlay prompting to accept cookies', () => <OverlayCookie />);
