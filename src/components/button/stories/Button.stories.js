e22b15174b7256a3fc18403f20a0ecc9db2c360e
Nitish Phanse <nitsums@gmail.com>
2018-10-16 00:41:39 +0530

    Feature/button components (#138)
    * added css styles for button types
    
    * added helpers for storybook
    
    * modified eslint config to add babel  eslint as parser
    
    * variables for button color
    
    * added button sizes styles
    
    * stories for buttons
    
    * test cases
    
    * added css styles for button types
    
    * added helpers for storybook
    
    * modified eslint config to add babel  eslint as parser
    
    * variables for button color
    
    * added button sizes styles
    
    * stories for buttons
    
    * test cases
    
    * incorrect component reference fixed
    
    * logging dir path
    
    * fixed case insensitivity issue on mack
    
    * decresed transiotion animation time
    
    * updated button styles
    
    * add proptypes
    
    * renamed to Button.js
    
    * renamed button folder
    
    * added teriary story
    
    * fixed type in tertiary story
    
    * added docs for button component
    
    * removed unused variables

---
+++ src/components/button/stories/Button.stories.js
@@ -0,0 +1,97 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebook, faTwitter, faInstagram } from '@fortawesome/free-brands-svg-icons';
+import Container from './container-story';
+import { withDocs } from 'storybook-readme';
+import ButtonReadme from './button.README.md';
+
+import Button from './../Button';
+import styles from './styles.scss';
+storiesOf('Buttons', module).add(
+  'Button Types',
+  withDocs(ButtonReadme, () => (
+    <Container title="Button Types">
+      <Button disabled>Disabled Button</Button>
+      <Button type="danger">Danger Button</Button>
+      <Button>Primary Button</Button>
+      <Button type="secondary">Secondary Button</Button>
+      <Button type="tertiary">Tertiary Button</Button>
+    </Container>
+  )),
+);
+
+storiesOf('Buttons', module).add('Button Sizes', () => (
+  <div>
+    <Container title="Primary Button Sizes">
+      <Button>Default Size</Button>
+      <Button size="large">Large Button</Button>
+      <Button size="small">Small Button</Button>
+    </Container>
+
+    <Container title="Secondary Button Sizes">
+      <Button type="secondary">Default Size</Button>
+      <Button type="secondary" size="large">
+        Large Button
+      </Button>
+      <Button type="secondary" size="small">
+        Small Button
+      </Button>
+    </Container>
+
+    <Container title="Tertiary Button Sizes">
+      <Button type="tertiary">Default Size</Button>
+      <Button type="tertiary" size="large">
+        Large Button
+      </Button>
+      <Button type="tertiary" size="small">
+        Small Button
+      </Button>
+    </Container>
+
+    <Container title="Danger Button Sizes">
+      <Button type="danger">Default Size</Button>
+      <Button type="danger" size="large">
+        Large Button
+      </Button>
+      <Button type="danger" size="small">
+        Small Button
+      </Button>
+    </Container>
+
+    <Container title="Disabled Button Sizes">
+      <Button disabled>Default Size</Button>
+      <Button disabled size="large">
+        Large Button
+      </Button>
+      <Button disabled size="small">
+        Small Button
+      </Button>
+    </Container>
+
+    <Container title="Full Width Buttons">
+      <Button type="primary" fullWidth>
+        Default Size
+      </Button>
+      <Button type="secondary" fullWidth>
+        Default Size
+      </Button>
+      <Button type="danger" fullWidth>
+        Default Size
+      </Button>
+    </Container>
+  </div>
+));
+
+storiesOf('Buttons', module).add('Button Composites', () => (
+  <div>
+    <Container title="Button Composites">
+      <Button>
+        <FontAwesomeIcon icon={faTwitter} size="2x" />
+        With Some Icon
+      </Button>
+
+      <Button onClick={() => alert('You clickedMe')}>Click</Button>
+    </Container>
+  </div>
+));
