2998ba3ed53989893cdf3e2ed4cf4c9a6fdc2651
Lena Venable <venablena@gmail.com>
2018-10-17 09:43:05 -0700

    remove unnecessary tests

--- src/pages/home/__tests__/HomeSection.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from 'react-testing-library';
-import HomeSection from '../components/HomeSection';
-import config from 'config/constants';
-
-test('renders the home page section', () => {
-  const { container } = render(<HomeSection />);
-  expect(container.firstChild).toMatchSnapshot();
-});
