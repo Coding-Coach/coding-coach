f32e877966d669ba6d200a7272fb76c7022e94b3
Crysfel Villa <crysfel@bleext.com>
2018-10-23 11:28:22 -0500

    Adding the donate button component

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,28 +1,14 @@
 import React from 'react';
 import { translate } from 'react-i18next';
-import Config from 'config/constants';
 import 'i18n/i18n';
-
-import Image from 'components/image/Image';
-
+import DonateButton from './DonateButton';
 import styles from './footer.scss';
 
 const Footer = ({ t }) => (
   <footer className={styles.footerMain}>
     <div className={styles.footerInner}>
       <div className={styles.column}>
-        <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
-          <input type="hidden" name="cmd" value="_s-xclick" />
-          <input type="hidden" name="hosted_button_id" value={Config.payments.paypal} />
-          <input
-            type="image"
-            src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif"
-            border="0"
-            name="submit"
-            alt="PayPal - The safer, easier way to pay online!"
-          />
-          <Image alt="Donate!" src="https://www.paypalobjects.com/de_DE/i/scr/pixel.gif" />
-        </form>
+        <DonateButton />
       </div>
       <div className={styles.column}>
         <a href="#terms" className={styles.footerLink}>
