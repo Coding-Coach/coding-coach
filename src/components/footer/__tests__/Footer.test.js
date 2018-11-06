79c0eafbbf5a3885b06fb3f587e55b97a56dab2e
Adaware Nero <neroadaware@gmail.com>
2018-11-06 13:52:24 +0100

    Fix/footer links (#211)
    * Fix/change Cookie policy to button
    
    * Changed footer links to Button coponent
    
    * changed text typography of the cookie overlay link
    
    * Added none option to Button's typography prop

--- src/components/footer/__tests__/Footer.test.js
+++ src/components/footer/__tests__/Footer.test.js
@@ -6,9 +6,4 @@ test('renders the footer element', () => {
   const { container } = render(<Footer />);
 
   expect(container.firstChild).toMatchSnapshot();
-
-  const href = container.querySelectorAll('a');
-  expect(href[0].getAttribute('href')).toEqual('#terms');
-  expect(href[1].getAttribute('href')).toEqual('#cookies');
-  expect(href[2].getAttribute('href')).toEqual('#privacy');
 });
