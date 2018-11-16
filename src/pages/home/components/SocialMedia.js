dea328e29cd20bf14a3a5d33d2ba4ee2c26010fd
nero <nero2009@rocketmail.com>
2018-11-16 15:49:44 +0100

    Migrate Social Media component from Scss to tailwind

--- src/pages/home/components/SocialMedia.js
+++ src/pages/home/components/SocialMedia.js
@@ -3,24 +3,35 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faFacebook, faTwitter, faInstagram } from '@fortawesome/free-brands-svg-icons';
 
 import config from 'config/constants';
-import styles from '../assets/socialmedia.module.scss';
 
 const SocialMedia = () => {
   return (
-    <div className={styles.socialMedia}>
-      <div className={styles.iconWrapper}>
+    <div className="flex flex-row-reverse self-center md:self-auto">
+      <div className="group m-3 p-4 rounded-full border border-solid border-primary-light cursor-pointer hover:bg-primary-light transition transition-fast transition-property-all transition-timing-linear">
         <a target="_blank" href={config.social.FB_URL} rel="noreferrer noopener">
-          <FontAwesomeIcon icon={faFacebook} size="2x" className={styles.greenIcons} />
+          <FontAwesomeIcon
+            icon={faFacebook}
+            size="2x"
+            className="text-primary-light group-hover:text-white transition transition-fast transition-property-all transition-timing-linear"
+          />
         </a>
       </div>
-      <div className={styles.iconWrapper}>
+      <div className="group m-3 p-4 rounded-full border border-solid border-primary-light cursor-pointer hover:bg-primary-light transition transition-fast transition-property-all transition-timing-linear">
         <a target="_blank" href={config.social.INSTA_URL} rel="noreferrer noopener">
-          <FontAwesomeIcon icon={faInstagram} size="2x" className={styles.greenIcons} />
+          <FontAwesomeIcon
+            icon={faInstagram}
+            size="2x"
+            className="text-primary-light group-hover:text-white transition transition-fast transition-property-all transition-timing-linear"
+          />
         </a>
       </div>
-      <div className={styles.iconWrapper}>
+      <div className="group m-3 p-4 rounded-full border border-solid border-primary-light cursor-pointer hover:bg-primary-light transition transition-fast transition-property-all transition-timing-linear">
         <a target="_blank" href={config.social.TWITTER_URL} rel="noreferrer noopener">
-          <FontAwesomeIcon icon={faTwitter} size="2x" className={styles.greenIcons} />
+          <FontAwesomeIcon
+            icon={faTwitter}
+            size="2x"
+            className="text-primary-light group-hover:text-white transition transition-fast transition-property-all transition-timing-linear"
+          />
         </a>
       </div>
     </div>
