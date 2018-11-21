c8e667d90b0bcaac21b5884a9f6b6c4530405d31
Crysfel Villa <crysfel@bleext.com>
2018-11-21 07:18:24 -0600

    Adding the panel for login

--- src/pages/auth/Login.js
+++ src/pages/auth/Login.js
@@ -1,17 +1,38 @@
 import React, { Fragment } from 'react';
 import { Trans, t } from '@lingui/macro';
+import { noop } from 'utils/noop';
 
+import Button from 'components/button/Button';
 import Image from 'components/image/Image';
+import { Panel, PanelContent } from 'components/panel/Panel';
 
 import logo from '../home/assets/images/coding-coach-logo.svg';
 
 export default function Login() {
   return (
     <div className="h-screen bg-gradient-primary px-4 py-3">
-      <Image className="w-12" src={logo} />
-      <h1 className="text-white font-semibold">
-        <Trans id="site.title" defaults="Coding Coach" />
-      </h1>
+      <div className="flex justify-between mb-12">
+        <Image className="w-12 h-8" src={logo} />
+        <Button onClick={noop} shape="pill" size="small" typography="none">
+          Sign up
+        </Button>
+      </div>
+      <div className="px-2">
+        <h1 className="text-white font-semibold mb-2">
+          <Trans id="site.title" defaults="Coding Coach" />
+        </h1>
+        <p className="text-white font-sm w-3/4 mb-4">
+          Sign in or create a new account to get started.
+        </p>
+        <Panel>
+          <PanelContent>
+            <h2 className="text-xl font-semibold text-primary">Sign In</h2>
+            <Button onClick={noop} size="small" typography="none" fullWidth>
+              Let's go!
+            </Button>
+          </PanelContent>
+        </Panel>
+      </div>
     </div>
   );
 }
