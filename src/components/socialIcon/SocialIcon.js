4a8f243f4eea3b4c6d6345499682cf2301c4d93a
nero <nero2009@rocketmail.com>
2018-11-29 14:56:33 +0100

    removed custom css file

--- src/components/socialIcon/SocialIcon.js
+++ src/components/socialIcon/SocialIcon.js
@@ -2,7 +2,6 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import './SocialIcon.css';
 
 /**
  *
