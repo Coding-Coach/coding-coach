ef2bf75600d51ff5953a389564eb2e97ecee3a3f
Alex Luong <aluong@crimson.ua.edu>
2018-12-09 19:04:39 -0600

    Close button only displays on hover

--- src/components/notification/Notification.js
+++ src/components/notification/Notification.js
@@ -10,63 +10,73 @@ const NOTIFICATION_TYPES = {
   error: 'error',
 };
 
-/**
- *
- * @param {type} props  mentorship, account, error
- * @param {title} props title string
- * @param {message} props  message string
- * @param {time} props  time string
- */
-const Notification = (props) => {
-  const { type, title, message, time, onClick, onClose } = props;
+class Notification extends React.Component {
+  state = { isActive: false };
 
-  let colorType;
-
-  switch (type) {
-    case NOTIFICATION_TYPES.account:
-      colorType = 'primary-light';
-      break;
-    case NOTIFICATION_TYPES.error:
-      colorType = 'danger-light';
-      break;
-    case NOTIFICATION_TYPES.mentorship:
-    default:
-      colorType = 'blue';
+  getColorType(type) {
+    switch (type) {
+      case NOTIFICATION_TYPES.account:
+        return 'primary-light';
+      case NOTIFICATION_TYPES.error:
+        return 'danger-light';
+      case NOTIFICATION_TYPES.mentorship:
+      default:
+        return 'blue';
+    }
   }
 
-  return (
-    <div
-      onClick={onClick}
-      className={classNames(
-        'text-left bg-white p-3 border-solid border-l-4 cursor-pointer relative',
-        `border-${colorType}`,
-        'transition-property-shadow transition-slow transition-timing-ease',
-        'hover:shadow-md focus:shadow-md',
-      )}
-    >
-      <h3 className="text-sm font-titles text-secondary-dark mb-2">{title}</h3>
-      <p className="text-xs font-thin font-content text-black mb-2">{message}</p>
-      <div className="text-right text-xs font-thin font-content text-secondary-lighter">{time}</div>
+  toggleActive = () => {
+    this.setState((prev) => ({ isActive: !prev.isActive }));
+  };
 
+  render() {
+    const { type, title, message, time, onClick, onClose } = this.props;
+    const { isActive } = this.state;
+    console.log(isActive);
+
+    const colorType = this.getColorType(type);
+
+    return (
       <div
+        onClick={onClick}
         className={classNames(
-          'absolute w-8 h-8 pin-t pin-r',
-          'flex items-center justify-center',
-          'bg-danger-light text-white rounded-full',
+          'group',
+          'text-left bg-white p-3 border-solid border-l-4 cursor-pointer relative',
+          `border-${colorType}`,
           'transition-property-shadow transition-slow transition-timing-ease',
           'hover:shadow-md focus:shadow-md',
         )}
-        style={{ transform: 'translate(50%, -50%)' }}
-        onClick={(e) => {
-          e.stopPropagation();
-          onClose && onClose(e);
-        }}
+        onMouseEnter={this.toggleActive}
+        onMouseLeave={this.toggleActive}
       >
-        <FontAwesomeIcon icon={faTimes} className="w-4 h-4 fill-current" />
+        <h3 className="text-sm font-titles text-secondary-dark mb-2">{title}</h3>
+        <p className="text-xs font-thin font-content text-black mb-2">{message}</p>
+        <div className="text-right text-xs font-thin font-content text-secondary-lighter">
+          {time}
+        </div>
+
+        {isActive && (
+          <div
+            className={classNames(
+              'absolute w-8 h-8 pin-t pin-r',
+              'flex items-center justify-center',
+              'bg-danger-light text-white rounded-full',
+              'transition-property-shadow transition-slow transition-timing-ease',
+              'hover:shadow-md focus:shadow-md',
+            )}
+            style={{ transform: 'translate(50%, -50%)' }}
+            onClick={(e) => {
+              e.stopPropagation();
+              onClose && onClose(e);
+            }}
+          >
+            <FontAwesomeIcon icon={faTimes} className="w-4 h-4 fill-current" />
+          </div>
+        )}
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 Notification.propTypes = {
   type: PropTypes.oneOf(Object.values(NOTIFICATION_TYPES)),
