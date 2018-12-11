920b9cd8b99b76faf8ec1a2fb2eda8af183a5d67
Crysfel Villa <crysfel@bleext.com>
2018-12-11 12:24:21 -0600

    Migrating the main layout for each section

--- src/pages/home/components/HomeSection.js
+++ src/pages/home/components/HomeSection.js
@@ -1,23 +1,37 @@
 import React from 'react';
-import { node, string } from 'prop-types';
-import styles from './HomeSection.module.scss';
+import { node, oneOf, string } from 'prop-types';
+import classNames from 'classnames';
 
-const HomeSection = ({ id, title, text, media, children }) => (
-  <section id={id} className={styles.section}>
-    <div className={styles.sectionInner}>
-      <div>
-        <header className={styles.header}>
-          <h1 className={styles.title}>{title}</h1>
-        </header>
-        {styles.description && <p className={styles.description}>{text}</p>}
-        {children}
+const HomeSection = ({ align, id, title, text, media, children }) => {
+  const isRight = align === 'right';
+
+  return (
+    <section id={id} className="py-32">
+      <div className="container flex">
+        <div
+          className={classNames('flex-1', { 'pl-8': isRight, 'pr-8': !isRight })}
+          style={{ order: isRight ? 2 : 1 }}
+        >
+          <h2
+            className={`text-primary-light text-5xl font-titles font-normal uppercase leading-tight text-${align}`}
+          >
+            {title}
+          </h2>
+          <p className={`text-lg text-secondary-dark leading-normal font-content text-${align}`}>
+            {text}
+          </p>
+          {children}
+        </div>
+        <div className="flex items-center w-2/5" style={{ order: isRight ? 1 : 2 }}>
+          {media}
+        </div>
       </div>
-      {media}
-    </div>
-  </section>
-);
+    </section>
+  );
+};
 
 HomeSection.propTypes = {
+  align: oneOf(['right', 'left']).isRequired,
   id: string.isRequired,
   title: string.isRequired,
   text: string,
--- tailwind.js
+++ tailwind.js
@@ -167,7 +167,7 @@ module.exports = {
     '2xl': '1.5rem',    // 24px
     '3xl': '1.875rem',  // 30px
     '4xl': '2.25rem',   // 36px
-    '5xl': '3rem',      // 48px
+    '5xl': '4.25rem',   // 68px
     '6xl': '5.25rem',   // 84px
   },
 
