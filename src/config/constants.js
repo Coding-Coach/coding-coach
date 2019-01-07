73ab8ba7e633f12b32a466f19daea6254f90579d
Matthew Sweeney <26944716+msweeneydev@users.noreply.github.com>
2019-01-07 21:14:43 +0000

    remove instagram from coding-coach (#281)

--- src/config/constants.js
+++ src/config/constants.js
@@ -7,7 +7,6 @@ export default {
   },
   social: {
     FB_URL: process.env.REACT_APP_FACEBOOK_URL || 'https://www.facebook.com/codingcoachio/',
-    INSTA_URL: process.env.REACT_APP_INSTA_URL || 'https://www.instagram.com/codingcoach_io/',
     TWITTER_URL: process.env.REACT_APP_TWITTER_URL || 'https://twitter.com/codingcoach_io',
     GITHUB_URL: process.env.REACT_APP_GITHUB_URL || 'https://github.com/Coding-Coach/coding-coach',
   },
