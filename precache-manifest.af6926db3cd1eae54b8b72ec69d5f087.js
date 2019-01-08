46d763dddc3d610b2c17f5157f624bd03e863942
Crysfel Villa <crysfel@bleext.com>
2019-01-08 11:04:38 -0600

    Updating

---
+++ precache-manifest.af6926db3cd1eae54b8b72ec69d5f087.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "f00fc8ded3f31746dd242d6fce29fd07",
+    "url": "/static/media/about.f00fc8de.svg"
+  },
+  {
+    "revision": "3e87327bcfcd81f5b1eb",
+    "url": "/static/css/main.32b3225b.chunk.css"
+  },
+  {
+    "revision": "c2b772bbf69365f59a63",
+    "url": "/static/js/1.c2b772bb.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "6c67e12ddf1d9a8c63f909a1f5c90160",
+    "url": "/static/media/PrivacyPolicy.6c67e12d.md"
+  },
+  {
+    "revision": "c8ebff2d4eb1969a8ce8ea29267fda9f",
+    "url": "/static/media/CookiesPolicy.c8ebff2d.md"
+  },
+  {
+    "revision": "49f2cd99bc00609ad8dec3a4c3ef801e",
+    "url": "/static/media/TermsAndConditions.49f2cd99.md"
+  },
+  {
+    "revision": "b6509b130a2cea995f1157617c4b807c",
+    "url": "/static/media/coding-coach-logo.b6509b13.svg"
+  },
+  {
+    "revision": "3e87327bcfcd81f5b1eb",
+    "url": "/static/js/main.3e87327b.chunk.js"
+  },
+  {
+    "revision": "2681c6b26439191b80fe490994861555",
+    "url": "/static/media/mission.2681c6b2.svg"
+  },
+  {
+    "revision": "052cb3b0c9df522f3b0fa1952d45a08c",
+    "url": "/static/media/contact.052cb3b0.svg"
+  },
+  {
+    "revision": "e10d0661a19f4b3fe8f5552d5759df00",
+    "url": "/static/media/fjalla-one-v5-latin_latin-ext-regular.e10d0661.woff2"
+  },
+  {
+    "revision": "96a1ada5a2657ab821d1111afafc8bac",
+    "url": "/static/media/fjalla-one-v5-latin_latin-ext-regular.96a1ada5.woff"
+  },
+  {
+    "revision": "e78dce533ecee30c5efd812bb23c248d",
+    "url": "/static/media/open-sans-v15-cyrillic-ext_vietnamese_cyrillic_greek_greek-ext_latin_latin-ext-regular.e78dce53.woff2"
+  },
+  {
+    "revision": "86f83cc557017c273dd44fa39d78a341",
+    "url": "/static/media/open-sans-v15-cyrillic-ext_vietnamese_cyrillic_greek_greek-ext_latin_latin-ext-regular.86f83cc5.woff"
+  },
+  {
+    "revision": "dade9050d52107b98461b7e13fc1446f",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,34 @@
-"use strict";var precacheConfig=[["/index.html","9251943cc0612003e445dcb9378e7dc4"],["/static/js/main.053b7599.js","a5c8d9bf9c09543f0e07af0fbdddd564"],["/static/media/about.f00fc8de.svg","f00fc8ded3f31746dd242d6fce29fd07"],["/static/media/coding-coach-logo.b6509b13.svg","b6509b130a2cea995f1157617c4b807c"],["/static/media/contact.052cb3b0.svg","052cb3b0c9df522f3b0fa1952d45a08c"],["/static/media/fjalla-one-v5-latin_latin-ext-regular.96a1ada5.woff","96a1ada5a2657ab821d1111afafc8bac"],["/static/media/fjalla-one-v5-latin_latin-ext-regular.e10d0661.woff2","e10d0661a19f4b3fe8f5552d5759df00"],["/static/media/lato-v14-latin_latin-ext-regular.b9277906.woff2","b92779064727b510ce76fd764912092a"],["/static/media/lato-v14-latin_latin-ext-regular.f18cac26.woff","f18cac26f977bb8997aff048107e5f02"],["/static/media/mission.2681c6b2.svg","2681c6b26439191b80fe490994861555"],["/static/media/open-sans-v15-cyrillic-ext_vietnamese_cyrillic_greek_greek-ext_latin_latin-ext-regular.86f83cc5.woff","86f83cc557017c273dd44fa39d78a341"],["/static/media/open-sans-v15-cyrillic-ext_vietnamese_cyrillic_greek_greek-ext_latin_latin-ext-regular.e78dce53.woff2","e78dce533ecee30c5efd812bb23c248d"]],cacheName="sw-precache-v3-sw-precache-webpack-plugin-"+(self.registration?self.registration.scope:""),ignoreUrlParametersMatching=[/^utm_/],addDirectoryIndex=function(e,t){var n=new URL(e);return"/"===n.pathname.slice(-1)&&(n.pathname+=t),n.toString()},cleanResponse=function(t){return t.redirected?("body"in t?Promise.resolve(t.body):t.blob()).then(function(e){return new Response(e,{headers:t.headers,status:t.status,statusText:t.statusText})}):Promise.resolve(t)},createCacheKey=function(e,t,n,a){var r=new URL(e);return a&&r.pathname.match(a)||(r.search+=(r.search?"&":"")+encodeURIComponent(t)+"="+encodeURIComponent(n)),r.toString()},isPathWhitelisted=function(e,t){if(0===e.length)return!0;var n=new URL(t).pathname;return e.some(function(e){return n.match(e)})},stripIgnoredUrlParameters=function(e,n){var t=new URL(e);return t.hash="",t.search=t.search.slice(1).split("&").map(function(e){return e.split("=")}).filter(function(t){return n.every(function(e){return!e.test(t[0])})}).map(function(e){return e.join("=")}).join("&"),t.toString()},hashParamName="_sw-precache",urlsToCacheKeys=new Map(precacheConfig.map(function(e){var t=e[0],n=e[1],a=new URL(t,self.location),r=createCacheKey(a,hashParamName,n,/\.\w{8}\./);return[a.toString(),r]}));function setOfCachedUrls(e){return e.keys().then(function(e){return e.map(function(e){return e.url})}).then(function(e){return new Set(e)})}self.addEventListener("install",function(e){e.waitUntil(caches.open(cacheName).then(function(a){return setOfCachedUrls(a).then(function(n){return Promise.all(Array.from(urlsToCacheKeys.values()).map(function(t){if(!n.has(t)){var e=new Request(t,{credentials:"same-origin"});return fetch(e).then(function(e){if(!e.ok)throw new Error("Request for "+t+" returned a response with status "+e.status);return cleanResponse(e).then(function(e){return a.put(t,e)})})}}))})}).then(function(){return self.skipWaiting()}))}),self.addEventListener("activate",function(e){var n=new Set(urlsToCacheKeys.values());e.waitUntil(caches.open(cacheName).then(function(t){return t.keys().then(function(e){return Promise.all(e.map(function(e){if(!n.has(e.url))return t.delete(e)}))})}).then(function(){return self.clients.claim()}))}),self.addEventListener("fetch",function(t){if("GET"===t.request.method){var e,n=stripIgnoredUrlParameters(t.request.url,ignoreUrlParametersMatching),a="index.html";(e=urlsToCacheKeys.has(n))||(n=addDirectoryIndex(n,a),e=urlsToCacheKeys.has(n));var r="/index.html";!e&&"navigate"===t.request.mode&&isPathWhitelisted(["^(?!\\/__).*"],t.request.url)&&(n=new URL(r,self.location).toString(),e=urlsToCacheKeys.has(n)),e&&t.respondWith(caches.open(cacheName).then(function(e){return e.match(urlsToCacheKeys.get(n)).then(function(e){if(e)return e;throw Error("The cached response that was expected is missing.")})}).catch(function(e){return console.warn('Couldn\'t serve response for "%s" from cache: %O',t.request.url,e),fetch(t.request)}))}});
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/precache-manifest.af6926db3cd1eae54b8b72ec69d5f087.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
