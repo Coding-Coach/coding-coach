5029c29e554cf6671fa48300db476c50b9bf264c
Trent Boyd <trent.boyd@gmail.com>
2019-01-09 21:50:06 -0600

    check-in for CCLogo and Dashboard icons

---
+++ src/components/icon/Icons.stories.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import CCLogo from './CCLogo';
+import Dashboard from './Dashboard';
+
+storiesOf('Icons', module)
+  .add('CCLogo', () => <CCLogo height={80} width={80} />)
+  .add('Dashboard active', () => <Dashboard height={80} width={80} active />)
+  .add('Dashboard inactive', () => <Dashboard height={80} width={80} inactive />);
