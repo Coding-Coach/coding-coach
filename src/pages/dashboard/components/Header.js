c837c6ba43246bc23b4c2b97ff371bb7c9e3a034
Victor Ribero <victor.ribero3@gmail.com>
2019-01-14 20:35:54 +0100

    removed classnames from Header Dashboard

--- src/pages/dashboard/components/Header.js
+++ src/pages/dashboard/components/Header.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import classNames from 'classnames';
 import { Trans } from '@lingui/macro';
 import logoSrc from './assets/logo.svg';
 
 function TopHeader() {
   return (
-    <div className={classNames('bg-gradient-primary h-48 py-5', 'md:py-6 md:h-64 lg:py-8')}>
+    <div className={'bg-gradient-primary h-48 py-5 md:py-6 md:h-64 lg:py-8'}>
       <div className="flex justify-between items-center w-5/6 m-auto">
-        <span className={classNames('text-white text-2xl font-bold')}>
+        <span className="text-white text-2xl font-bold">
           <Trans id="dashboard.header.title" />
         </span>
         <img src={logoSrc} className="w-12" />
@@ -28,7 +27,7 @@ function Header({ banner }) {
       {banner && (
         <div
           style={{ marginTop: '-6rem' }}
-          className={classNames('mx-auto w-5/6 p-8', 'bg-white shadow', 'md:w-3/5 xl:w-2/5')}
+          className={'mx-auto w-5/6 p-8 bg-white shadow md:w-3/5 xl:w-2/5'}
         >
           {banner}
         </div>
