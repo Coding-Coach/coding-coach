101238bb4f99d1b891cab42da8b4a598c8dc59eb
Victor Ribero <victor.ribero3@gmail.com>
2019-01-14 20:38:01 +0100

    Use of Image Component

--- src/pages/dashboard/components/Header.js
+++ src/pages/dashboard/components/Header.js
@@ -2,15 +2,16 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Trans } from '@lingui/macro';
 import logoSrc from './assets/logo.svg';
+import Image from 'components/Image';
 
 function TopHeader() {
   return (
-    <div className={'bg-gradient-primary h-48 py-5 md:py-6 md:h-64 lg:py-8'}>
+    <div className="bg-gradient-primary h-48 py-5 md:py-6 md:h-64 lg:py-8">
       <div className="flex justify-between items-center w-5/6 m-auto">
         <span className="text-white text-2xl font-bold">
           <Trans id="dashboard.header.title" />
         </span>
-        <img src={logoSrc} className="w-12" />
+        <Image src={logoSrc} className="w-12" alt="Logo of Coading Coach" />
       </div>
     </div>
   );
@@ -27,7 +28,7 @@ function Header({ banner }) {
       {banner && (
         <div
           style={{ marginTop: '-6rem' }}
-          className={'mx-auto w-5/6 p-8 bg-white shadow md:w-3/5 xl:w-2/5'}
+          className="mx-auto w-5/6 p-8 bg-white shadow md:w-3/5 xl:w-2/5"
         >
           {banner}
         </div>
