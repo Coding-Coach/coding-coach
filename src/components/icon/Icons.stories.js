ae82878d94d72bd1287554972e86bfae854d7263
Trent Boyd <trent.boyd@gmail.com>
2019-01-24 13:47:03 -0600

    updates to Dashboard icon

--- src/components/icon/Icons.stories.js
+++ src/components/icon/Icons.stories.js
@@ -3,7 +3,7 @@ import { storiesOf } from '@storybook/react';
 import CCLogo from './CCLogo';
 import Dashboard from './Dashboard';
 
-storiesOf('Icons', module)
+storiesOf('Components/Icons', module)
   .add('CCLogo', () => <CCLogo height={80} width={80} />)
   .add('Dashboard active', () => <Dashboard height={80} width={80} active />)
-  .add('Dashboard inactive', () => <Dashboard height={80} width={80} inactive />);
+  .add('Dashboard inactive', () => <Dashboard height={80} width={80} />);
