3e7f9df1432f0380ffd7873e97b3d16522c1ca0f
kriyeng <kriyeng@gmail.com>
2019-02-08 20:31:17 +0100

    New component to scroll page to top when users scrolls down. Fixes #130

---
+++ src/pages/home/components/ScrollTopButton.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import Button from 'components/button/Button';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowUp } from '@fortawesome/free-solid-svg-icons';
+
+class ScrollTopButton extends React.Component {
+  state = { scrollTopClass: '' };
+
+  componentDidMount() {
+    this.scrollHandler();
+    window.addEventListener('scroll', this.scrollHandler);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('scroll', this.scrollHandler);
+  }
+
+  scrollHandler = () => {
+    this.setState({
+      scrollToTopClass: window.pageYOffset > window.innerHeight / 4 ? 'opacity-100' : '',
+    });
+  };
+
+  clickHandler = () => {
+    window.scrollTo({
+      behavior: 'smooth',
+      top: 0,
+    });
+  };
+
+  render() {
+    const styles = {
+      hide: {
+        bottom: 20,
+        right: -65,
+      },
+      show: {
+        right: 20,
+      },
+    };
+    const { scrollToTopClass } = this.state;
+    return (
+      <div
+        aria-label="Scroll to top of page"
+        tabIndex={scrollToTopClass ? 0 : -1}
+        className={`transition-slow fixed cursor-pointer opacity-0 z-50 ${scrollToTopClass}`}
+        style={{
+          ...styles.hide,
+          ...(scrollToTopClass && styles.show),
+        }}
+      >
+        <Button onClick={this.clickHandler} onKeyPress={this.clickHandler}>
+          <FontAwesomeIcon icon={faArrowUp} size="2x" />
+        </Button>
+      </div>
+    );
+  }
+}
+export default ScrollTopButton;
