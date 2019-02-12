b69f840f89d89cf21d647e63278d6da097ed0240
Victor Ribero <victor.ribero3@gmail.com>
2019-02-12 22:39:55 +0100

    removed return keyword in favor of arrow syntax implicit return

--- src/pages/dashboard/components/NavBar.js
+++ src/pages/dashboard/components/NavBar.js
@@ -4,18 +4,16 @@ function NavBar({ configuration, ...rest }) {
   return (
     <nav className="fixed pin-b pin-x bg-white md:pin-y ">
       <ul role="tablist" className="list-reset flex px-0 md:px-3 lg:px-4">
-        {configuration.tabs.map((tabConfig) => {
-          return (
-            <Tab
-              key={tabConfig.name}
-              name={tabConfig.name}
-              to={tabConfig.path}
-              isSelected={tabConfig.selected}
-              label={tabConfig.label}
-              icon={<tabConfig.icon className="w-12" active={tabConfig.selected} />}
-            />
-          );
-        })}
+        {configuration.tabs.map((tabConfig) => (
+          <Tab
+            key={tabConfig.name}
+            name={tabConfig.name}
+            to={tabConfig.path}
+            isSelected={tabConfig.selected}
+            label={tabConfig.label}
+            icon={<tabConfig.icon className="w-12" active={tabConfig.selected} />}
+          />
+        ))}
       </ul>
     </nav>
   );
