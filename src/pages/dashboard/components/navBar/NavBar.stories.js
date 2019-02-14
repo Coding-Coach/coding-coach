484099e1e8fbf3ed4780cfa3b4dd5d0f2d3a5f2f
Victor Ribero <victor.ribero3@gmail.com>
2019-02-14 12:22:00 +0100

    Updated stories

--- src/pages/dashboard/components/navBar/NavBar.stories.js
+++ src/pages/dashboard/components/navBar/NavBar.stories.js
@@ -7,32 +7,65 @@ import IconDashboard from 'components/icon/Dashboard';
 
 storiesOf('App / NavBar / Mobile', module)
   .addDecorator(withViewport('iphone5'))
-  .add('Only 1 tab', () => <NavBar configuration={singleTabConfiguration} />)
-  .add('More than 1 tab', () => <NavBar configuration={multipleTabConfiguration} />);
+  .add('Only 1 tab', () => <NavBar configuration={singleTabConfiguration} currentPath="one" />)
+  .add('More than 1 tab', () => (
+    <NavBar configuration={multipleTabConfiguration} currentPath="one" />
+  ));
 
 storiesOf('App / NavBar / iPad', module)
   .addDecorator(withViewport('ipad'))
-  .add('Only 1 tab', () => <NavBar configuration={singleTabConfiguration} />)
-  .add('More than 1 tab', () => <NavBar configuration={multipleTabConfiguration} />);
+  .add('Only 1 tab', () => <NavBar configuration={singleTabConfiguration} currentPath="one" />)
+  .add('More than 1 tab', () => (
+    <NavBar configuration={multipleTabConfiguration} currentPath="one" />
+  ));
 
 storiesOf('App / NavBar / Tablet and larger', module)
-  .add('Only 1 tab', () => <NavBar configuration={singleTabConfiguration} />)
-  .add('More than 1 tab', () => <NavBar configuration={multipleTabConfiguration} />);
+  .add('Only 1 tab', () => <NavBar configuration={singleTabConfiguration} currentPath="one" />)
+  .add('More than 1 tab', () => (
+    <NavBar configuration={multipleTabConfiguration} currentPath="one" />
+  ));
 
 const singleTabConfiguration = {
-  tabs: [0].map((element) => ({
-    name: `name ${element}`,
-    label: `Label for ${element}`,
-    path: '/fake_path',
-    icon: IconDashboard,
-  })),
+  tabs: [
+    {
+      name: 'name_one',
+      group: 'one',
+      label: 'label one',
+      path: '/fake_path',
+      icon: IconDashboard,
+    },
+  ],
 };
 
 const multipleTabConfiguration = {
-  tabs: [0, 1, 2, 3].map((element) => ({
-    name: `name ${element}`,
-    label: `Label for ${element}`,
-    path: '/fake_path',
-    icon: IconDashboard,
-  })),
+  tabs: [
+    {
+      name: 'name_one',
+      group: 'one',
+      label: 'label one',
+      path: '/fake_path',
+      icon: IconDashboard,
+    },
+    {
+      name: 'name_two',
+      group: 'two',
+      label: 'label two',
+      path: '/fake_path',
+      icon: IconDashboard,
+    },
+    {
+      name: 'name_three',
+      group: 'three',
+      label: 'label three',
+      path: '/fake_path',
+      icon: IconDashboard,
+    },
+    {
+      name: 'name_four',
+      group: 'four',
+      label: 'label four',
+      path: '/fake_path',
+      icon: IconDashboard,
+    },
+  ],
 };
