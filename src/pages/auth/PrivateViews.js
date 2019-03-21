f920751b0336c3c167c70f4a5678522e945f03e4
Crysfel Villa <crysfel@bleext.com>
2019-03-20 20:02:25 -0500

    Adding styles to the error page

--- src/pages/auth/PrivateViews.js
+++ src/pages/auth/PrivateViews.js
@@ -1,7 +1,9 @@
 import React, { Component, Fragment } from 'react';
 import { object } from 'prop-types';
+import { Trans } from '@lingui/macro';
 import NavBar from 'pages/dashboard/components/navBar/NavBar';
 import Button from 'components/button/Button';
+import { Panel, PanelContent } from 'components/panel/Panel';
 
 export default class PrivateViews extends Component {
   static propTypes = {
@@ -36,9 +38,22 @@ export default class PrivateViews extends Component {
     }
 
     return (
-      <div>
-        <p>I'm sorry but you can't be here</p>
-        <Button onClick={auth.login}>Login</Button>
+      <div className="flex items-center justify-center h-screen bg-gradient-primary">
+        <div className="px-2 md:w-3/5 lg:w-1/3">
+          <Panel floating>
+            <PanelContent>
+              <p>
+                <Trans
+                  id="auth.signin.forbiden"
+                  defaults="You are not authorized. Please sign in to access this page."
+                />
+              </p>
+              <Button onClick={auth.login}>
+                <Trans id="auth.signin.loginRegister" defaults="Login or Register" />
+              </Button>
+            </PanelContent>
+          </Panel>
+        </div>
       </div>
     );
   }
