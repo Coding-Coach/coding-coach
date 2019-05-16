a68de669b53774694ed0f1e95182fffea5a643da
Brent M Clark <brentmclark@gmail.com>
2019-05-16 09:51:16 -0500

    ensure we're on the root before we anchor link

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -26,13 +26,13 @@ function Navbar() {
         <NavLink to="/openings/frontend-jr">
           <Trans id="home.mentors.openings" />
         </NavLink>
-        <NavLink to="#about">
+        <NavLink to="/#about">
           <Trans id="home.about.title" />
         </NavLink>
-        <NavLink to="#mission" className="hidden md:block">
+        <NavLink to="/#mission" className="hidden md:block">
           <Trans id="home.mission.title" />
         </NavLink>
-        <NavLink to="#contact" className="hidden md:block">
+        <NavLink to="/#contact" className="hidden md:block">
           <Trans id="home.contact.title" />
         </NavLink>
       </div>
