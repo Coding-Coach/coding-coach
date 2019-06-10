7630391bf47a0aa53c3abe9714bb1bbb4d342867
jpggvilaca <jpggvilaca@gmail.com>
2019-06-10 12:49:29 +0200

    Delete unused import

--- src/pages/home/__tests__/home.test.js
+++ src/pages/home/__tests__/home.test.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { render } from 'react-testing-library';
 import { I18nProvider } from '@lingui/react';
-import { t } from '@lingui/macro';
 import { catalogs } from 'config/i18n';
 import Home from '../Home';
 
