081094e92b8eb342d84d1e5b16c1d95282caa70a
Crysfel Villa <crysfel@bleext.com>
2019-08-28 23:42:32 -0500

    Fixing purgecss to avoid removing styles from blog posts

--- gatsby-config.js
+++ gatsby-config.js
@@ -70,7 +70,8 @@ if (process.env.NODE_ENV === 'production') {
       printRejected: true, // Print removed selectors and processed file names
       // develop: true, // Enable while using `gatsby develop`
       tailwind: true, // Enable tailwindcss support
-      // whitelist: ['whitelist'], // Don't remove this selector
+      whitelist: ['.blog-content'], // Don't remove this selector
+      whitelistPatternsChildren: [/^pre/, /^code/], 
       // ignore: ['/ignored.css', 'prismjs/', 'docsearch.js/'], // Ignore files/folders
       // purgeOnly : ['components/', '/main.css', 'bootstrap/'], // Purge only these files/folders
     }
