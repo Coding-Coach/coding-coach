60976dd8d42685e33ccf945a0da1755877a7fc36
Crysfel Villa <crysfel@bleext.com>
2019-12-12 10:25:01 -0600

    Updating slack link (#365)

--- src/pages/index.js
+++ src/pages/index.js
@@ -50,7 +50,7 @@ export default function IndexPage() {
           We want to hear your thoughts! Feel free to join our
           <a
             className="inline-block mx-2 text-black border-b border-black no-underline hover:bg-primary-light hover:text-white hover:border-primary-light"
-            href="https://join.slack.com/t/coding-coach/shared_invite/enQtNDYxNTcwMjk4MDcwLWQ2MWEwYTc3YTZmMmM5ZDJjOGRkMDg0NGRjOWJhZThlZjUyMmViYzM4YjY4MjFjZGJkNjAzNDVlYTEwMWY2ZTU"
+            href="https://join.slack.com/t/coding-coach/shared_invite/enQtODYwODU0NDE2OTkzLTRmMTY3NTZmOGI2MjYyZTM2ZDBlZTNjNmMwNGRhY2I3MzZkMjA3ZjlhZjliYmQzMGQyYTkxNTViZTc3MzJmOTU"
           >
             Slack Organization
           </a>
