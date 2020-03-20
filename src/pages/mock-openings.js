5082a529f79244d6862c9abf06930a981bb48e4d
shayanypn <shayanypn@gmail.com>
2020-03-20 19:40:00 +0330

    remove table border and links text-decoration

--- src/pages/mock-openings.js
+++ src/pages/mock-openings.js
@@ -9,9 +9,9 @@ const MockOpenings = () => (
   <Fragment>
     <SEO title="Mock Openings" />
     <MainNav title="Mock Openings" />
-    <div className="container py-16 blog-content md:flex">
+    <div className="container py-16 md:flex">
       <div className="flex-1 md:pl-16">
-        <p>
+        <p className="py-3">
           In Coding Coach beside our open-source application,  we have an opportunity for all developers to try a mock job opportunity. This way developers can learn the common process of applying for a job, how to prepare C.V. and cover letters, different steps of the process, and estimate themself and their knowledge during the different rules. We are defining a company named CodingCoach CO. as our fake company.
         </p>
 
@@ -32,33 +32,36 @@ const MockOpenings = () => (
           </thead>
           <tbody>
             <tr>
-              <td className="border px-4 py-2">1</td>
-              <td className="border px-4 py-2">Front-End</td>
-              <td className="border px-4 py-2">Jr. Front-end Engineer</td>
-              <td className="border px-4 py-2">Junior, Mid-Level</td>
-              <td className="border px-4 py-2">
-                <Button className="uppercase shadow-lg" to="/mock-openings/jr-frontend-engineer/">
+              <td className="px-4 py-2">1</td>
+              <td className="px-4 py-2">Front-End</td>
+              <td className="px-4 py-2">Jr. Front-end Engineer</td>
+              <td className="px-4 py-2">Junior, Mid-Level</td>
+              <td className="px-4 py-2">
+                <Button
+                  className="uppercase shadow-lg" 
+                  to="/mock-openings/jr-frontend-engineer/"
+                >
                   Apply
                 </Button>
               </td>
             </tr>
             <tr className="bg-gray-100">
-              <td className="border px-4 py-2">2</td>
-              <td className="border px-4 py-2">Front-End</td>
-              <td className="border px-4 py-2">Senior Front-end Developer, React Focus</td>
-              <td className="border px-4 py-2">Mid-Level, Senior</td>
-              <td className="border px-4 py-2">
+              <td className="px-4 py-2">2</td>
+              <td className="px-4 py-2">Front-End</td>
+              <td className="px-4 py-2">Senior Front-end Developer, React Focus</td>
+              <td className="px-4 py-2">Mid-Level, Senior</td>
+              <td className="px-4 py-2">
                 <Button className="uppercase shadow-lg" to="/mock-openings/senior-frontend-developer-react-focus/">
                   Apply
                 </Button>
               </td>
             </tr>
             <tr>
-              <td className="border px-4 py-2">3</td>
-              <td className="border px-4 py-2">Back-end</td>
-              <td className="border px-4 py-2">Back-end Node.Js developer</td>
-              <td className="border px-4 py-2">Junior, Mid-Level</td>
-              <td className="border px-4 py-2">
+              <td className="px-4 py-2">3</td>
+              <td className="px-4 py-2">Back-end</td>
+              <td className="px-4 py-2">Back-end Node.Js developer</td>
+              <td className="px-4 py-2">Junior, Mid-Level</td>
+              <td className="px-4 py-2">
                 <Button className="uppercase shadow-lg" to="/mock-openings/backend-nodejs-developer/">
                   Apply
                 </Button>
