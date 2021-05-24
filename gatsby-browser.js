dea5c957bd90e583a032a07a3592c148149275af
Mosh Feu <moshfeu@gmail.com>
2021-05-24 11:57:35 +0300

    feat: add tawk

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -5,3 +5,14 @@
  */
 
 import './src/assets/css/app.css';
+
+var Tawk_API = Tawk_API || {},
+  Tawk_LoadStart = new Date();
+(function() {
+  var s1 = document.createElement('script'),
+    s0 = document.getElementsByTagName('script')[0];
+  s1.async = true;
+  s1.src = 'https://embed.tawk.to/60a117b2185beb22b30dae86/1f5qk953t';
+  s1.setAttribute('crossorigin', '*');
+  s0.parentNode.insertBefore(s1, s0);
+})();
